1834d4cb777c4c4bd9ac217f85c202edd9cc87d0
Alex Lee <arl7816@g.rit.edu>
2026-09-20 15:41:53 -0400

    Gets everything set up for log in

--- mem-dine-frontend/project-mem-dash/src/App.vue
+++ mem-dine-frontend/project-mem-dash/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import LogIn from './components/LogIn.vue';
 </script>
 
 <template>
   <header>
-   
+    <LogIn />
   </header>
 </template>
 
